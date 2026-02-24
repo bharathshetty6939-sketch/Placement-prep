2b7f5f90bb9c234c3e22415e314b2879411a88eb
Bharathkumar <bharathshetty6939@gmail.com>
2026-02-24 18:04:21 +0530

    day2 coding completed

---
+++ day2.py
@@ -0,0 +1,20 @@
+# --- DAY 2: VARIABLES AND DATA TYPES ---
+
+# 1. String (Text)
+name = "bharathkumar" 
+
+# 2. Integer (Whole Numbers)
+days_total = 365
+days_completed = 1
+
+# 3. Float (Decimal Numbers)
+progress_percentage = 0.27  # (1/365 * 100)
+
+# 4. Boolean (True or False)
+is_consistent = True
+
+# Printing them all together
+print(f"Student Name: {name}")
+print(f"Progress: {days_completed}/{days_total} days")
+print(f"Consistency Status: {is_consistent}")
+print(f"progress_percentage:{progress_percentage}")
