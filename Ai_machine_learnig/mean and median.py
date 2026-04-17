cfce47dc5d0795277f02a40ab300d63c51da23bf
Bharathkumar <bharathshetty6939@gmail.com>
2026-04-17 21:19:16 +0530

    Day12/mean and median

---
+++ Ai_machine_learnig/mean and median.py
@@ -0,0 +1,6 @@
+import numpy as np
+data=np.array([10, 20, 20, 30, 40, 50])
+mean=np.mean(data)
+median=np.median(data)
+print("mean of the given data is:",mean)
+print("Median of the given data is:",median)
