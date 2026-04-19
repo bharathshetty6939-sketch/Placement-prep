8123c04e060e200d21c156543116c272c450ba47
Bharathkumar <bharathshetty6939@gmail.com>
2026-04-19 21:40:55 +0530

    Day43/Displaying linked list value using while loop.

---
+++ linked_list_display.py
@@ -0,0 +1,14 @@
+class node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+node1=node(10)
+node2=node(20)
+node3=node(30)
+node1.next=node2
+node2.next=node3
+current = node1  
+
+while current is not None:
+    print("Visiting node with data:", current.data)
+    current = current.next 
