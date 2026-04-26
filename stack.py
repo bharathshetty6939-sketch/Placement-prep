46dde800e2576997ae7eaa3a5ddb743bb3e8f4de
Bharathkumar <bharathshetty6939@gmail.com>
2026-04-26 21:28:29 +0530

    Day44/Stack logic

---
+++ stack.py
@@ -0,0 +1,9 @@
+stack=[]
+stack.append(1)
+stack.append(2)
+stack.append(3)
+
+print("stack after push",stack)
+popped_element=stack.pop()
+print("popped element is",popped_element)
+print("stack after pop operation",stack)
