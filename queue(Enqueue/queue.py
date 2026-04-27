84fcc44f804b3ac89332ac9e92095335a8d178e3
Bharathkumar <bharathshetty6939@gmail.com>
2026-04-27 20:47:53 +0530

    Day 45/Learnt about queue operation..

---
+++ queue(Enqueue/queue.py
@@ -0,0 +1,8 @@
+queue=[]
+queue.append("bharath")
+queue.append("Keshav")
+queue.append("suchit")
+print("intial queue",queue)
+removed_person=queue.pop(0)
+print("Removed person from queue is",removed_person)
+print("queue afer removing person is",queue)
